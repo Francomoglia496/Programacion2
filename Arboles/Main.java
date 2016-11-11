fdc9cd1d967cd72606b227655fcb4e7cddbf60ce
Francomoglia496 <francomoglia496@gmail.com>
2016-11-11 22:14:19 +0200

    Arbol Actualizado
    Arbol Actualizado

---
+++ Arboles/Main.java
@@ -0,0 +1,22 @@
+package Programacion2.Arboles;
+
+import java.util.Scanner;
+
+/**
+ * Created by francomoglia on 11/1/16.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        Arbol a = new Arbol(new Nodo(sc.nextInt()));
+
+        for (int i = 0; i < 5; i++) {
+            a.addNodo(new Nodo(sc.nextInt()));
+        }
+
+        a.BFSArbol(a.getRaiz());
+    }
+}
